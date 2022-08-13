dcfb517313d42418096f6bb16647869c9a87bf40
sanju <sanjusk08gmail.com>
2022-08-13 17:40:13 +0530

    Green: Add next_generation() along with test

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
-def get_neighbours_count(matrix, row, col):
-    row_size_limit = len(matrix)
-    col_size_limit = len(matrix[0])
+def neighbours_count(matrix, row, col):
+    row_max = len(matrix) - 1
+    col_max = len(matrix[0]) - 1
     alive_cell = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
@@ -11,10 +11,28 @@ def get_neighbours_count(matrix, row, col):
             if (
                 next_row < 0
                 or next_col < 0
-                or next_row > row_size_limit
-                or next_col > col_size_limit
+                or next_row > row_max
+                or next_col > col_max
             ):
                 continue
+            print(next_row, next_col)
             if matrix[next_row][next_col]:
                 alive_cell += 1
     return alive_cell
+
+
+def next_generation(matrix):
+    new_matrix = [[ False for j in range(len(matrix[0]))] for i in range(len(matrix))]
+    row_max = len(matrix)
+    col_max = len(matrix[0])
+
+    for row in range(row_max):
+        for col in range(col_max):
+            if neighbours_count(matrix, row, col) in (2 ,3) and matrix[row][col] == True:
+                new_matrix[row][col] = True
+            elif neighbours_count(matrix, row, col) == 3 and matrix[row][col] == False:
+                new_matrix[row][col] = True
+            else:
+                new_matrix[row][col] = False 
+    return new_matrix
+
--- test_logic.py
+++ test_logic.py
@@ -1,22 +1,27 @@
 import logic
 
 
-def test_get_neighbours_count():
+def test_neighbours_count():
     matrix = [[False for col in range(3)] for row in range(3)]
-    assert logic.get_neighbours_count(matrix, 1, 1) == 0
+    assert logic.neighbours_count(matrix, 1, 1) == 0
     matrix[0][0] = True
-    assert logic.get_neighbours_count(matrix, 1, 1) == 1
+    assert logic.neighbours_count(matrix, 1, 1) == 1
     matrix[0][1] = True
-    assert logic.get_neighbours_count(matrix, 1, 1) == 2
+    assert logic.neighbours_count(matrix, 1, 1) == 2
     matrix[0][2] = True
-    assert logic.get_neighbours_count(matrix, 1, 1) == 3
+    assert logic.neighbours_count(matrix, 1, 1) == 3
     matrix[1][0] = True
-    assert logic.get_neighbours_count(matrix, 1, 1) == 4
+    assert logic.neighbours_count(matrix, 1, 1) == 4
     matrix[1][2] = True
-    assert logic.get_neighbours_count(matrix, 1, 1) == 5
+    assert logic.neighbours_count(matrix, 1, 1) == 5
     matrix[2][0] = True
-    assert logic.get_neighbours_count(matrix, 1, 1) == 6
+    assert logic.neighbours_count(matrix, 1, 1) == 6
     matrix[2][1] = True
-    assert logic.get_neighbours_count(matrix, 1, 1) == 7
+    assert logic.neighbours_count(matrix, 1, 1) == 7
     matrix[2][2] = True
-    assert logic.get_neighbours_count(matrix, 1, 1) == 8
+    assert logic.neighbours_count(matrix, 1, 1) == 8
+
+def test_next_generation():
+    matrix = [[False, True, False], [False, True, False], [False, True, False],]
+    next_gen = [[False, False, False], [True, True, True], [False, False, False], ]
+    assert logic.next_generation(matrix) == next_gen
