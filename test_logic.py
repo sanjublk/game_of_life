fc662cf821ec3e97d3e095afea834c06d1e2b1c3
sanju <sanjusk08gmail.com>
2022-08-18 10:19:43 +0530

    added comments for test_neighbours_count

--- test_logic.py
+++ test_logic.py
@@ -3,6 +3,7 @@ import logic
 
 def test_neighbours_count():
     matrix = [[False for col in range(3)] for row in range(3)]
+    # testing number of neighbours of matrix[1][1]
     assert logic.neighbours_count(matrix, 1, 1) == 0
     matrix[0][0] = True
     assert logic.neighbours_count(matrix, 1, 1) == 1
@@ -20,6 +21,8 @@ def test_neighbours_count():
     assert logic.neighbours_count(matrix, 1, 1) == 7
     matrix[2][2] = True
     assert logic.neighbours_count(matrix, 1, 1) == 8
+
+
 
 
 def test_next_generation():
