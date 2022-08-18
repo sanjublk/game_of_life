112be1bbb740d400c15b6b9689aab2bececcec9f
sanju <sanjusk08gmail.com>
2022-08-18 10:46:42 +0530

    added __main__.py with parser skeleton

---
+++ __main__.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Conway's Game of Life")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    
+
+
+
